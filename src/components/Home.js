85d0389784ddcf6510059ab8f728767df4f05968
jiraiya1729 <128409206+jiraiya1729@users.noreply.github.com>
2023-10-07 21:20:43 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
       </div>
       <div className="mt-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-xl p-6 border-2 border-white rounded-lg shadow-md bg-opacity-70 bg-white flex flex-col items-center text-center">
         <FaRocket className="text-6xl mb-4 text-gray-700" />
-        <p className="text-2xl text-gray-700">Welcome to elspace</p>
+        <p className="text-2xl text-gray-700">Welcome to elfspace</p>
         <p className="text-xl text-gray-500">Your aerospace standard technical assistance</p>
       </div>
     </div>
