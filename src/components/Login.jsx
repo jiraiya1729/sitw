2b6ab4cca3910121b213a6a9ed0a251e970256a2
jiraiya1729 <128409206+jiraiya1729@users.noreply.github.com>
2023-10-07 22:22:05 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,9 +3,6 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
 export const Login = (props) => {
-  // const [namew, setNamew] = useState("");
-  // const [agew, setAgew] = useState("");
-  // const [numberw, setNumberw] = useState("");
 
     
         const [action, setAction] = useState("Sign Up");
@@ -87,9 +84,7 @@ export const Login = (props) => {
               localStorage.setItem('number',loginform.number)
               localStorage.setItem('name',loginform.name)
               localStorage.setItem('age',loginform.age)
-              // localStorage.setItem('email',loginform.number)
-              // localStorage.setItem('email',loginform.number)
-              // setAction("Login")
+              
       }).catch((error) => {
           if (error.response){
               console.log(error.response)
@@ -114,31 +109,7 @@ export const Login = (props) => {
 
     
   return (
-//     <div  className='grid grid-cols-1 sm:grid-cols-1 h-screen w-full'>
-//  {/* <div className="hidden sm:block ">
 
-//     <img className="w-full h-full object-cover" src={loginImg} alt=""/>
-//      </div> */}
-
-//     <div className='bg-gray-800 flex flex-col justify-center h-full'>
-//         <form className='max-w-[400px] w-full  mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
-//             <h2 className='text-4xl dark:text-white font-bold text-center'>SIGN IN</h2>
-//             <div className='flex flex-col text-gray-400 py-2'>
-//                 <label>user Name</label>
-//                 <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800focus:outline-none' type="text" />
-//             </div>
-//             <div className='flex flex-col text-gray-400 py-2'> 
-//                 <label>Password</label>
-//                 <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800focus:outline-none' type="text"/>
-//             </div>
-//            <div className='flex justify-between text-gray-400 py-2'>
-//             <p className='flex item-center'><input className='mr-2' type="checkbox" />Remember Me</p>
-//             <p>Forgot Password</p>
-//            </div>
-//             <button className='w-full my-5 py-2 bg-teal-500 shadow-teal-500/50 text-white font-semibold rounded-lg'>Sign In</button>
-//         </form>
-//     </div>
-//    </div>
 <div className="min-h-screen py-40" style={{backgroundImage: 'linear-gradient(115deg, #9F7AEA, #81D4FA)'}}>
   <div className="container mx-auto">
     <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
